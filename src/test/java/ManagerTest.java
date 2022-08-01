124543d5dfa817b40b3c010c7635f40dcdf65d09
Artem4812 <artem.malchenko.1995@mail.ru>
2022-08-01 16:07:26 +0400

    Second

--- src/test/java/ManagerTest.java
+++ src/test/java/ManagerTest.java
@@ -41,4 +41,127 @@ public class ManagerTest {
         Assertions.assertArrayEquals(expected, actual);
     }
 
+    @Test
+
+    public void ShouldGetId() {
+        int expected = 1;
+        int actual = ticket1.getId();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldSetId() {
+        ticket1.setId(8);
+        int expected = 8;
+        int actual = ticket1.getId();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void ShouldGetPrice() {
+        int expected = 10_000;
+        int actual = ticket1.getPrice();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldSetPrice() {
+        ticket1.setPrice(100_000);
+        int expected = 100_000;
+        int actual = ticket1.getPrice();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldGetFrom() {
+        String expected = "ABA";
+        String actual = ticket1.getFrom();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldSetFrom() {
+        ticket1.setFrom("ABS");
+        String expected = "ABS";
+        String actual = ticket1.getFrom();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldGetTo() {
+        String expected = "KUF";
+        String actual = ticket1.getTo();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldSetTo() {
+        ticket1.setTo("RSA");
+        String expected = "RSA";
+        String actual = ticket1.getTo();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldGetTime() {
+        String expected = "15:00";
+        String actual = ticket1.getTime();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldSetTime() {
+        ticket1.setTime("19:00");
+        String expected = "19:00";
+        String actual = ticket1.getTime();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldAddTicket() {
+        repository.save(ticket6);
+        Ticket[] expected = {ticket5, ticket2, ticket1, ticket4, ticket3, ticket6};
+        Ticket[] actual = repository.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+
+    public void ShouldRemoveById() {
+        repository.removeById(3);
+        Ticket[] expected = {ticket5, ticket2, ticket1, ticket4};
+        Ticket[] actual = repository.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+    public void ShouldFindById() {
+        Ticket expected = ticket5;
+        Ticket actual = repository.findById(5);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldNotFindById() {
+        Ticket expected = null;
+        Ticket actual = repository.findById(15);
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+
+
+
+
+
 }
